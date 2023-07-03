4b554df7fa1faacb885165e5abb80097b10827bd
constantineiljinyh <122673134+constantineiljinyh@users.noreply.github.com>
2023-07-03 16:00:23 +0500

    initialize maven project

---
+++ src/main/java/ru/yandex/practicum/filmorate/JavafilmorateApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.filmorate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavafilmorateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavafilmorateApplication.class, args);
+	}
+
+}
